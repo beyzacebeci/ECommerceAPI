9926fdd824559f976638f5dcf68f3e4851d7b2cb
Beyza Cebeci <beyzacebeci34@gmail.com>
2024-11-04 21:32:20 +0300

    added gitignore file

--- Presentation/ECommerceAPI.API/obj/Debug/net8.0/ECommerceAPI.API.AssemblyInfo.cs
+++ Presentation/ECommerceAPI.API/obj/Debug/net8.0/ECommerceAPI.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerceAPI.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98aa3d395127b12eb3de1eef222d07941205411a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerceAPI.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerceAPI.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
