fee0f4017ce19167ba485326b50afb77966d5750
Beyza Cebeci <beyzacebeci34@gmail.com>
2024-11-08 16:28:20 +0300

    ProductService has been created and registered in IoC.

--- Presentation/ECommerceAPI.API/obj/Debug/net8.0/ECommerceAPI.API.AssemblyInfo.cs
+++ Presentation/ECommerceAPI.API/obj/Debug/net8.0/ECommerceAPI.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerceAPI.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98aa3d395127b12eb3de1eef222d07941205411a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b751456259f22e2971f471744d3a9a0bbe3edc9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerceAPI.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerceAPI.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
